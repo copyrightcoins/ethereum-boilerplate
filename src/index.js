00c88940130da7fc4472fa873b37acbc1f254124
Internet Music <internetmusic@users.noreply.github.com>
2021-11-25 12:33:23 +0100

    Inti

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { MoralisProvider } from "react-moralis";
+import {MoralisProvider} from "react-moralis";
 import "./index.css";
 import QuickStart from "components/QuickStart";
-import { MoralisDappProvider } from "./providers/MoralisDappProvider/MoralisDappProvider";
+import {MoralisDappProvider} from "./providers/MoralisDappProvider/MoralisDappProvider";
 
 /** Get your free Moralis Account https://moralis.io/ */
 
-const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
-const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+const APP_ID = "vYOgIMso0FV6bq1YRg4Ew3qmfuJhOb2Yy5c2xCvW";
+const SERVER_URL = "https://j0lecmwqvhvo.usemoralis.com:2053/server";
 
 const Application = () => {
   const isServerInfo = APP_ID && SERVER_URL ? true : false;
